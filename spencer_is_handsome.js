07a096fb7d0c18cbdd58b4b222e0cfde314edc7e
Spencer Smitherman <spencer.smitherman@gmail.com>
2014-10-20 23:13:10 -0700

    Adding functions/refactoring

--- spencer_is_handsome.js
+++ spencer_is_handsome.js
@@ -1,12 +1,29 @@
-function printToConsole() {
-	if (window.optimizely.activeExperiments) {
-		for (var i = 0; window.optimizely.activeExperiments > i; i++) {
-			console.log("Optimizely Active Experiments: " + window.optimizely.activeExperiments[i]);
-			console.log("Optimizely Variations: " + window.optimizely.variationNamesMap[window.optimizely.activeExperiments[i]]);
-		}
+function snippetLocation() {
+	if ($('head').find($('script[src*="//cdn.optimizely.com/"]')).length > 0) {
+		console.log("OK: The snippet is in the <head>.")
+	} else if ($('body').find($('script[src*="//cdn.optimizely.com/"]')).length > 0) {
+		console.log("Warning: The snippet is in the <body>.")
 	} else {
-		console.log("There are no active experiments on this page.")
+		console.log("The snippet could not be found on the page.")
 	}
-
 }
+
+function experimentInfo() {
+	if (window.optimizely !== undefined) {
+	
+		if (window.optimizely.activeExperiments.length > 0) {
+			for (var i = 0; window.optimizely.activeExperiments.length > i; i++) {
+				console.log("Optimizely Active Experiment: " + window.optimizely.activeExperiments[i] + " : " + window.optimizely.variationNamesMap[window.optimizely.activeExperiments[i]]);
+			}
+		} else {
+			console.log("There are no active experiments on this page.")
+		}
+		console.log("Optimizely Revision: " + window.optimizely.revision)
+	}
+}
+
+function printToConsole() {
+	snippetLocation();
+}
+
 printToConsole();
